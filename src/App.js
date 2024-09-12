5ac8fb99ed33164879eaf6e9629212e726758f35
Jonathan Delgado <jonathan.degado6@gmail.com>
2024-09-11 23:47:27 -0600

    create-components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import useFormulario from './hooks/useFormulario'
+import Input from './components/Input'
+import Card from './components/Card'
+import Container from './components/Container'
+import Button from './components/Button'
+
 function App() {
-  const [formulario, handleChange] = useFormulario( { name: '' })
+  const [formulario, handleChange] = useFormulario({ 
+    name: '', 
+    email: '',
+    lastname: '',
+  })
   console.log(formulario)
   return (
-    <form>
-      <input
-       name="name"
-       placeholder="Nombre"
-       value={formulario.name}
-       onChange={handleChange}/>
-    </form>
-  );
+    <Container>
+      <Card>
+        <div style={{ padding:20 }}>
+          <form>
+            <Input 
+              label="Nombre" 
+              name="name" 
+              value={formulario.name} 
+              onChange={handleChange}
+            />
+            <Input 
+              label="Apellido"
+              name="lastname" 
+              value={formulario.lastname} 
+              onChange={handleChange}
+            />
+            <Input 
+              label="Correo"
+              name="email" 
+              value={formulario.email} 
+              onChange={handleChange}
+            />
+            <Button>Enviar</Button>
+          </form>
+        </div>
+      </Card>
+    </Container>
+  )
 }
 
-export default App;
+export default App
